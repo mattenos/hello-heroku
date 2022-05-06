ebbb901ec22eb552a700c352e0a896520ec3cc8d
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-05-06 11:44:18 -0400

    starter files

---
+++ src/main/java/com/example/HelloHeroku/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.HelloHeroku.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+//    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    @GetMapping(value="/hello")
+    @ResponseStatus(HttpStatus.OK)
+}
